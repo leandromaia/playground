6d37209d81a12302c83001c6766230e7fa0889c9
leandromaia <leandroc@inatel.br>
2014-02-17 17:17:39 -0300

    adding fielsets to forms

--- exercices-leandro/django/startsite/specificationproduct/admin.py
+++ exercices-leandro/django/startsite/specificationproduct/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from specificationproduct.models import FeatureValue
-from specificationproduct.models import Product
-from specificationproduct.models import Feature
+from specificationproduct.models import Product, Feature, FeatureValue
 
-admin.site.register(FeatureValue)
-admin.site.register(Product)
-admin.site.register(Feature)
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name']}),
+        ('Campos de Valores', {'fields': ['price','feature_value']}),
+    ]    
+
+class FeatureAdmin(admin.StackedInline):
+    fieldsets = [
+        ('Nome',               {'fields': ['name']}),
+        ('Descricao', {'fields': ['description']}),
+        ('Valor da Feature', {'fields': ['feature_value']})
+    ] 
+    model = Feature
+    extra = 1
+
+class FeatureValueAdmin(admin.ModelAdmin):
+    fieldsets = [        
+        ('Valor', {'fields': ['value']})
+    ]
+    inlines = [FeatureAdmin]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(FeatureValue, FeatureValueAdmin)
--- exercices-leandro/django/startsite/specificationproduct/models.py
+++ exercices-leandro/django/startsite/specificationproduct/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class FeatureValue(models.Model):
+    name = models.CharField(max_length=10)
     value = models.IntegerField(default=0)
 
 class Product(models.Model):
