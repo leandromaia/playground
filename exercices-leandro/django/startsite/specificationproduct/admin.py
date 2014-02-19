6d7fbfb3d12f0456a2b151c5508a03e0cdee72de
leandromaia <leandroc@inatel.br>
2014-02-19 11:02:17 -0300

    Finalization of specification product app from django exercices

--- exercices-leandro/django/startsite/specificationproduct/admin.py
+++ exercices-leandro/django/startsite/specificationproduct/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
-from specificationproduct.models import Product, FeatureValue 
+from specificationproduct.models import Feature, FeatureValue 
+from specificationproduct.models import Product, ProductSpec
 
-class ProductInLine(admin.StackedInline):    
-    model = Product
+class FeatureInLine(admin.StackedInline):    
+    model = Feature
     extra = 1
 
 class FeatureValueAdmin(admin.ModelAdmin):
     fieldsets = [        
         ('Valor', {'fields': ['value']})
     ]
-    inlines = [ProductInLine]
+    inlines = [FeatureInLine]
+    list_display = ['value']
+
+class ProductSpecInLine(admin.StackedInline):
+    fieldsets = [
+        (None, {'fields': ['name','feature']})
+    ]
+    model = ProductSpec
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Valores Basicos', {'fields': ['name','price', 'feature_value']})]
+    inlines = [ProductSpecInLine]
+    list_display = ('name', 'price','feature_value')
 
 admin.site.register(FeatureValue, FeatureValueAdmin)
+admin.site.register(Product, ProductAdmin)
