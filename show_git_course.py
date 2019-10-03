b4065ecafcbdc0e7f6bc57444ead447777a7ad15
Creso Jr <juninp_@live.com>
2019-10-03 20:43:11 -0300

    Alterado o valor de studente de Leandro Maia para Creso Jr

--- show_git_course.py
+++ show_git_course.py
@@ -1,9 +1,9 @@
-from datetime import date
+﻿from datetime import date
 
 print("Curso de GIT com código Python")
 
 today = date.today()
 format_date = today.strftime("%d-%m-%y")
-student = "Leandro Maia"
+student = "Creso Jr"
 
 print(f"{student} está fazendo o curso de GIT no dia {format_date}")
