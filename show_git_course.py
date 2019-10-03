ace36df18b6ae70290e7c15ed6859000b1b08257
Samuel Rodrigues Silva <samuka_55@hotmail.com>
2019-10-03 20:43:06 -0300

    student value changed for Samuel Silva

--- show_git_course.py
+++ show_git_course.py
@@ -4,6 +4,6 @@ print("Curso de GIT com código Python")
 
 today = date.today()
 format_date = today.strftime("%d-%m-%y")
-student = "Leandro Maia"
+student = "Samuel Silva"
 
 print(f"{student} está fazendo o curso de GIT no dia {format_date}")
